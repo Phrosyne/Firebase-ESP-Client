826ab6b82eb38f6843ef50b7e46b3dcddc917372
Phrosyne <mcoderej@gmail.com>
2026-05-12 11:25:58 -0700

    Ok this was kind of a big one. We made the file holding all the keys (hush.h) generalized so now the repo comes with a boilerplate that you can actually just edit directly. I realized that the previous configuration was unnecessarily confusing as you kind of had to intuit that there was a missing <hush.h> file and add it yourself. For presentation purposes we had it polling every 5 seconds, but now we've moved the code to setup() and implemented the actual deep sleep timer for a tentative period of 30 minutes which I think is reasonable, but it is pretty simple to change it. So now it should actually fulfill the power efficient part of our original scope. Feels gud man.

--- data/main.cpp
+++ data/main.cpp
@@ -4,17 +4,15 @@
 #include <Firebase_ESP_Client.h>
 #include <ArduinoJson.h>
 #include <HTTPClient.h>
-#include <hush.h>
+#include <hush.h.example>
 #include <HX711.h>
 
-#define uS_TO_S_FACTOR 1000000ULL
-#define TIME_TO_SLEEP 5
-
-String pass = WIFI_PASS;
+#define ms_To_S 1000000ULL
+#define seconds_To_Sleep 1800
 
 const String FIREBASE_API_KEY = FIREBASE_API;
 const String FIREBASE_PROJECT_ID = PROJECT_ID;
-const String COLLECTION_NAME = "sensor_readings";
+const String COLLECTION_NAME = COLLECTION;
 
 HX711 scale;
 const int LOADCELL_DOUT_PIN = 5;
@@ -30,7 +28,7 @@ void setup()
   scale.set_scale(450);
   scale.tare();
 
-  WiFi.disconnect(true); // Disconnect from any previous networks
+  WiFi.disconnect(true);
   WiFi.mode(WIFI_STA);
 
   // Configure WPA2 Enterprise Enterprise Identity and Password
@@ -42,8 +40,6 @@ void setup()
   esp_wifi_sta_wpa2_ent_enable();
 
   WiFi.begin(WIFI_SSID);
-
-  // WiFi.begin(HOME_SSID, HOME_PASS);
 
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED)
@@ -57,29 +53,30 @@ void setup()
   Serial.println();
 
   Serial.println("Ready:");
-  scale.tare();
-}
+  
+  double s1 = scale.get_units(1);
+  double s2 = scale.get_units(1);
+  double s3 = scale.get_units(1);
 
-void loop()
-{
-  if (scale.is_ready()) {
-    double s1 = scale.get_units(1);
-    double s2 = scale.get_units(1);
-    double s3 = scale.get_units(1);
-
-    double sensorValue;
-    if ((s2 <= s1 && s1 <= s3) || (s3 <= s1 && s1 <= s2)) {
-      sensorValue = s1;
-    } else if ((s1 <= s2 && s2 <= s3) || (s3 <= s2 && s2 <= s1)) {
-      sensorValue = s2;
-    } else {
-      sensorValue = s3;
-    }
-
-    sendDataToFirestore(sensorValue);
-    delay(5000);
+  double sensorValue;
+  if ((s2 <= s1 && s1 <= s3) || (s3 <= s1 && s1 <= s2)) {
+    sensorValue = s1;
+  } else if ((s1 <= s2 && s2 <= s3) || (s3 <= s2 && s2 <= s1)) {
+    sensorValue = s2;
+  } else {
+    sensorValue = s3;
   }
+
+  sendDataToFirestore(sensorValue);
+
+  //Deep sleep for half an hour
+  esp_sleep_enable_timer_wakeup(ms_To_S * seconds_To_Sleep);
+  Serial.flush();
+  WiFi.disconnect(true);
+  esp_deep_sleep_start();
 }
+
+void loop() {}
 
 void sendDataToFirestore(double food_level)
 {
@@ -89,8 +86,6 @@ void sendDataToFirestore(double food_level)
     return;
   }
 
-  // Assemble the Firestore REST API URL.
-  // By POSTing to the collection name, Firestore creates a random document ID.
   String url = "";
   url += "https://firestore.googleapis.com/v1/projects/";
   url += FIREBASE_PROJECT_ID;
@@ -99,8 +94,6 @@ void sendDataToFirestore(double food_level)
   url += "/Sensor1?key=";
   url += FIREBASE_API_KEY;
 
-  // Create the request payload (body) in the JSON format required by Firestore.
-  // This structure is the most critical part and a common source of errors.
   StaticJsonDocument<256> jsonDoc;
 
   JsonObject fields = jsonDoc.createNestedObject("fields");
@@ -111,7 +104,6 @@ void sendDataToFirestore(double food_level)
   String jsonPayload;
   serializeJson(jsonDoc, jsonPayload);
 
-  // Start the HTTP request
   HTTPClient http;
   http.begin(url);
   http.addHeader("Content-Type", "application/json");
@@ -122,25 +114,21 @@ void sendDataToFirestore(double food_level)
   Serial.print("Payload: ");
   Serial.println(jsonPayload);
 
-  // Send the POST request with the JSON payload
   int httpResponseCode = http.PATCH(jsonPayload);
 
-  // Check the server's response
-  if (httpResponseCode > 0)
-  {
-    String responsePayload = http.getString();
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    Serial.print("Response: ");
-    Serial.println(responsePayload);
-  }
-  else
-  {
-    Serial.print("Error on POST request. Error code: ");
-    Serial.println(httpResponseCode);
-  }
-
-  Serial.println("--- END OF HTTP REQUEST --- \n");
+  // if (httpResponseCode > 0)
+  // {
+  //   String responsePayload = http.getString();
+  //   Serial.print("HTTP Response code: ");
+  //   Serial.println(httpResponseCode);
+  //   Serial.print("Response: ");
+  //   Serial.println(responsePayload);
+  // }
+  // else
+  // {
+  //   Serial.print("Error on POST request. Error code: ");
+  //   Serial.println(httpResponseCode);
+  // }
 
   // Free resources
   http.end();
